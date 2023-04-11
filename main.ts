74d0b920a33737198688c3b4f4ee0926743ee8cb
Vince <45806875+vince1444@users.noreply.github.com>
2023-04-11 00:48:05 -0400

    Added try..catch block to grabToken() function

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
 const axios = require("axios");
 require("dotenv").config();
 
-const SPOTIFY_TOKEN_ENDPOINT: string = process.env
-  .SPOTIFY_TOKEN_ENDPOINT as string;
+const globalOptions = {
+  token: null,
+};
+const SPOTIFY_TOKEN_ENDPOINT: string = process.env.SPOTIFY_TOKEN_ENDPOINT as string;
 const SPOTIFY_CLIENT_ID: string = process.env.SPOTIFY_CLIENT_ID as string;
-const SPOTIFY_CLIENT_SECRET: string = process.env
-  .SPOTIFY_CLIENT_SECRET as string;
+const SPOTIFY_CLIENT_SECRET: string = process.env.SPOTIFY_CLIENT_SECRET as string;
 
-const grabToken = async (
-  endpoint: string,
-  clientId: string,
-  clientSecret: string
-): Promise<Object> => {
-  const token = await axios.post(
-    endpoint,
-    {
-      grant_type: "client_credentials",
-      client_id: clientId,
-      client_secret: clientSecret,
-    },
-    {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+const grabToken = async (endpoint: string, clientId: string, clientSecret: string): Promise<string> => {
+  // I was going to make a decorator for this but I think it might be better to allow each function to granularly handle errors
+  try {
+    const token = await axios.post(
+      endpoint,
+      {
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret,
       },
-    }
-  );
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
 
-  return token.data.access_token; // we only care about the access_token
+    // we only care about the access_token
+    return token.data.access_token;
+  } catch (error: any) {
+    throw new Error(`An error has occurred during ${grabToken.name}: ${error}`);
+  }
 };
 
-grabToken(SPOTIFY_TOKEN_ENDPOINT, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET);
+const x = grabToken(SPOTIFY_TOKEN_ENDPOINT, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+  .then((data) => console.log(data))
+  .catch((error) => console.log("ERROR"));
